43e2ecc9cd41a61b84c032560af08fa18ed5fe08
Franciszek Glombek <54372619+StingerSan@users.noreply.github.com>
2021-08-03 22:23:12 +0200

    Main gallery layout

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const DivOne = document.querySelector('div:nth-of-type(1)')
 const DivTwo = document.querySelector('div:nth-of-type(2)')
 const DivThree = document.querySelector('div:nth-of-type(3)')
 const DivFour = document.querySelector('div:nth-of-type(4)')
+const FrontPage = document.querySelector('.FrontPage')
 
 function animation() {
     ButtonClick.classList.add('BtnAnimation')
@@ -11,6 +12,7 @@ function animation() {
     DivTwo.classList.add('DivAnimationTwo')
     DivThree.classList.add('DivAnimationThree')
     DivFour.classList.add('DivAnimationFour')
+    FrontPage.classList.add('FrontPageAnimation')
 }
 ButtonClick.addEventListener('click', animation)
 //  ***** End of the front page animation *****
