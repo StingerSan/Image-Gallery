e189c84aab1ee1c8c861a6795eaa942b4d327dad
Franciszek Glombek <54372619+StingerSan@users.noreply.github.com>
2021-08-16 21:09:40 +0200

    Finished

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const DivFour = document.querySelector('div:nth-of-type(4)')
 const FrontPage = document.querySelector('.FrontPage')
 
 function animation() {
+    BckFrontPage.classList.remove('AnimTrigger')
     ButtonClick.classList.add('BtnAnimation')
     DivOne.classList.add('DivAnimationOne')
     DivTwo.classList.add('DivAnimationTwo')
@@ -21,6 +22,12 @@ const SpanClick = document.querySelector('span')
 const BckFrontPage = document.querySelector('.FrontPage')
 
 function animationTwo() {
+    ButtonClick.classList.remove('BtnAnimation')
+    DivOne.classList.remove('DivAnimationOne')
+    DivTwo.classList.remove('DivAnimationTwo')
+    DivThree.classList.remove('DivAnimationThree')
+    DivFour.classList.remove('DivAnimationFour')
+    FrontPage.classList.remove('FrontPageAnimation')
     BckFrontPage.classList.add('AnimTrigger')
 }
 SpanClick.addEventListener('click', animationTwo)
